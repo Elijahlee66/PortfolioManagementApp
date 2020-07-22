62675dd6ee99b70de2d151698fc6c67e21578cdc
Elijahlee66 <55210873+Elijahlee66@users.noreply.github.com>
2020-07-22 14:58:37 -0400

    update

--- MaxSharpeRatio.m
+++ MaxSharpeRatio.m
@@ -1,5 +1,5 @@
 function SharpeRatio = MaxSharpeRatio(Weight, Hist_Close, Rf)
-    Weight = abs(Weight);
+%     Weight = abs(Weight);
     PaperPortfolio = sum((Weight .* Hist_Close),2);
 
     TDays = size(PaperPortfolio,1);
@@ -13,7 +13,7 @@ function SharpeRatio = MaxSharpeRatio(Weight, Hist_Close, Rf)
     AveReturn = mean(DailyReturn);
     Volatility = std(DailyReturn);
     
-    if min(Weight) >= 1 && max(Weight) <= 30
+    if min(Weight) >= 0 && max(Weight) <= 30
         SharpeRatio = -((AveReturn-Rf)/Volatility);
     else
         SharpeRatio = 100;
